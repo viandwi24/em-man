125c75449fdf85e59ed683b4b21e6cfb53b44f6f
viandwi24 <fiandwi0424@gmail.com>
2020-06-14 21:06:48 +0700

    update db

---
+++ app/Models/Web.php
@@ -0,0 +1,11 @@
+<?php
+
+namespace App\Models;
+
+use Illuminate\Database\Eloquent\Model;
+
+class Web extends Model
+{
+    protected $table = 'web';
+    protected $guarded = [];
+}
--- database/seeds/DatabaseSeeder.php
+++ database/seeds/DatabaseSeeder.php
@@ -1,5 +1,6 @@
 <?php
 
+use App\Models\Web;
 use Illuminate\Database\Seeder;
 
 class DatabaseSeeder extends Seeder
@@ -11,6 +12,7 @@ class DatabaseSeeder extends Seeder
      */
     public function run()
     {
+        Web::create([]);
         $this->call(UsersTableSeeder::class);
     }
 }
