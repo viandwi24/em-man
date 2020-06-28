2b0f70a0da241a28e47acc3e0660fe02c79882ac
viandwi24 <fiandwi0424@gmail.com>
2020-06-29 00:41:50 +0700

    update

--- resources/views/components/alert-flash.blade.php
+++ resources/views/components/alert-flash.blade.php
@@ -15,10 +15,10 @@
 
     @if ($errors->any())
         <script>iziToast.error({ title: 'Error', message: 'Data tidak lengkap atau tidak sesuai format.', });</script>
-        @php $i=0; @endphp
+        {{-- @php $i=0; @endphp
         @foreach ($errors->all() as $error)
             @php if ($i == 1) { break; } else { $i++; } @endphp
             <script>iziToast.error({ title: 'Error', message: '{{ $error }}', });</script>
-        @endforeach
+        @endforeach --}}
     @endif
 @endpush
